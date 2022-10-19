a3e6bfb336eaeb83818ab12a5132aa6452be2b50
Rocko <codingwithrock@gmail.com>
2022-10-19 11:24:14 +0600

    added firebase app setup

---
+++ src/Firebase/Firebase.congif.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCgjwqs0evFVj7L-u4LZzEE2gFM7NLrFJ8",
+  authDomain: "email-auth-a25af.firebaseapp.com",
+  projectId: "email-auth-a25af",
+  storageBucket: "email-auth-a25af.appspot.com",
+  messagingSenderId: "766896844046",
+  appId: "1:766896844046:web:832480258409c66c233b5f"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);

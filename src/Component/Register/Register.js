a3dab004933cf183f6ef71481a6cdec460f05f43
Rocko <codingwithrock@gmail.com>
2022-10-20 12:04:47 +0600

    displayer set user name

--- src/Component/Register/Register.js
+++ src/Component/Register/Register.js
@@ -1,4 +1,4 @@
-import { createUserWithEmailAndPassword, getAuth, sendEmailVerification } from 'firebase/auth';
+import { createUserWithEmailAndPassword, getAuth, sendEmailVerification, updateProfile } from 'firebase/auth';
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { app } from '../../Firebase/Firebase.config';
@@ -19,7 +19,6 @@ const Register = () => {
     const [ErrorState, setErrorState] = useState('Please Set Password');
     //  Error End
 
-    const [currentUser, setCurrentUser] = useState();
 
 
 
@@ -30,6 +29,7 @@ const Register = () => {
 
         const email = event.target.email.value;
         const password = event.target.password.value;
+        const name = event.target.name.value;
         const form = event.target;
 
 
@@ -51,11 +51,13 @@ const Register = () => {
         }
 
 
-        const userVerify = ()=> {
+        const userVerify = () => {
 
-                sendEmailVerification(auth.currentUser)
-                .then(result=> console.log("Check Email!"))
-                .catch(error=> console.log(error))
+            sendEmailVerification(auth.currentUser)
+                .then(result => console.log(result))
+                .catch(error => console.log(error))
+
+                userNameUpdate(name);
 
         }
 
@@ -67,20 +69,36 @@ const Register = () => {
                 setErrorState("Please Check Your Email!");
                 form.reset();
                 userVerify();
-                setTimeout(()=> {
+                setTimeout(() => {
                     navigate('/');
-                },2000)
-                
+                }, 2000)
+
             })
             .catch(error => setErrorState(error.message));
 
 
 
-            
+
+            // User Name Update Method.
+        const userNameUpdate = (name)=> {
+
+                updateProfile(auth.currentUser, 
+                    { 
+                        displayName: name,
+                    })
+
+                    .then(result=> console.log("updated"))
+                    .catch(error => console.log(error));
+
+        }
 
 
-        
-        
+
+
+
+
+
+
 
 
 
@@ -92,7 +110,9 @@ const Register = () => {
             </h1>
 
             <form onSubmit={submitForm}>
-                <input required type="text" name='email' placeholder='Email' />
+                <input required type="text" name='name' placeholder='Name' />
+                <br />
+                <input required style={{ marginTop: '5px' }} type="text" name='email' placeholder='Email' />
                 <br />
                 <input required style={{ marginTop: '5px' }} name='password' type="text" placeholder='Password' />
                 <br />
